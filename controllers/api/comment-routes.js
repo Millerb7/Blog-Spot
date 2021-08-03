77de9ad9470dbab0bf062a1104c885fd2fe0beb3
Windmill <millerjboyd@gmail.com>
2021-08-03 17:45:42 -0500

    added most of routes data

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,3 +3,20 @@
 /* 
     /
 */
+
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.create({
+        ...req.body,
+        post_id: req.seesion.post_id,
+    });
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,3 +5,59 @@
     /:id
     /:id
 */
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require("../../utils/auth");
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = postData.user_id;
+        req.session.logged_in = true;
+        req.session.post_id = postData.id;
+  
+        res.status(200).json(postData);
+      });
+    } catch (err) {
+      res.status(404).json(err);
+    }
+});
+
+router.put('/:id', withAuth, async (req,res) => {
+    try {
+        const postData = await Post.update(
+            {where: {id: req.params.id}},
+            {body: req.body.body},
+            {title: req.body.title}
+        );
+
+        if(postData) {
+            res.status(200).json('data updated');
+        } else {
+            res.status(404).json('data incorrect');
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req,res) => {
+    try {
+        const postData = await Post.destroy({
+          where: {
+            id: req.params.id,
+            user_id: req.session.user_id,
+          },
+        });
+    
+        if (postData) {
+            res.status(200).json(projectData);
+        } else {
+            res.status(404).json({ message: 'No project found with this id!' });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
