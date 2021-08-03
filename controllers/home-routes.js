f5ef4354a28c5af36b61363e5d068ce2e7d09078
Windmill <millerjboyd@gmail.com>
2021-08-02 21:26:31 -0500

    added some code for routes, need to finish that

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,3 +5,66 @@
     /post/:id
     /login
 */
+
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+            model: User,
+            attributes: ['name'],
+        },
+        {
+            model: Comment,
+            attributes: ['body'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+            model: Comment,
+            attributes: ['body'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('singlepost', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', !withAuth, (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  res.render('login');
+});
+
+module.exports = router;
